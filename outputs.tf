fd28acd606148df3f72932f5ca3f241186b1e4c5
Piotr Wreczycki <netf@netf.io>
2020-04-14 19:28:01 +0200

    Added outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "cluster_id" {
+  value = aws_ecs_cluster.main.id
+}
+
+output "cluster_security_group_id" {
+  value = aws_security_group.main.id
+}
