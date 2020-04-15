89c2acb55135577b8e03fce5d9e7709d1dc6e62c
Piotr Wreczycki <netf@netf.io>
2020-04-15 19:07:05 +0200

    Fixed outputs, added gitignore and pre-commit hook

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
-  value = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.main[0].name
 }
 
 output "cluster_id" {
-  value = aws_ecs_cluster.main.id
+  value = aws_ecs_cluster.main[0].id
 }
 
 output "cluster_security_group_id" {
-  value = aws_security_group.main.id
+  value = aws_security_group.main[0].id
 }
