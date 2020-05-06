f6b97048ccc65d7a634ae6ef3f1bf1a2ef1393a1
Piotr Wreczycki <netf@netf.io>
2020-05-06 13:49:32 +0200

    Added wildcard to output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
-  value = aws_ecs_cluster.main[0].name
+  value = [aws_ecs_cluster.main.*.name]
 }
 
 output "cluster_id" {
-  value = aws_ecs_cluster.main[0].id
+  value = [aws_ecs_cluster.main.*.id]
 }
 
 output "cluster_security_group_id" {
-  value = aws_security_group.main[0].id
+  value = [aws_security_group.main.*.id]
 }
